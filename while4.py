025fdafd1090d9632e6a3ef7d3c5e10cd0d6445b
luisfelipe-fernandesesteves <luisfelipefernandes.corp@gmail.com>
2026-03-27 17:43:07 -0300

    variaveis declaradas

---
+++ while4.py
@@ -0,0 +1,16 @@
+'''Exercício 4 Média de valores 
+Escreva um programa que leia vários números informados pelo usuário. 
+O programa deve encerrar quando o usuário digitar -1 e, ao final, calcular e exibir a média dos valores digitados 
+
+(desconsiderando o -1). 
+Exemplo: 
+Digite um número (-1 para sair): 6 
+Digite um número (-1 para sair): 8 
+Digite um número (-1 para sair): 4 
+Digite um número (-1 para sair): -1 
+Média: 6.0
+'''
+numero = int(input("Digite um número (-1 para sair): "))
+soma = 0
+
+
