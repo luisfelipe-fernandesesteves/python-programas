bd50cec8a06d476b839ed9320cc976a613235ce4
luisfelipe-fernandesesteves <luisfelipefernandes.corp@gmail.com>
2026-03-27 18:04:10 -0300

    coodigo corrigido

--- while5.py
+++ while5.py
@@ -14,7 +14,16 @@ Digite a senha: 1234
 Acesso permitido!
 Bora praticar, galera! Ótimos estudos!!!
 '''
-senha = 1234
-
-while senha_digitada != senha:
-   senha_digitada = int(input("Digite a senha: "))
+senha_correta = "1234"
+# O programa continuará solicitando a senha até que o usuário acerte a senha correta.
+# O loop while é usado para repetir a solicitação de senha até que a condição seja satisfeita (senha correta).
+#WHILE TRUE em Python é uma estrutura de controle de fluxo que cria um loop infinito.
+# nao utilize o while true, pois ele pode causar um loop infinito se não for controlado adequadamente.
+while True:
+    senha_digitada = input("Digite a senha: ")
+    if senha_digitada == senha_correta:
+        print("Acesso permitido!")
+        #break é uma palavra-chave em Python que é usada para sair imediatamente de um loop, seja ele um loop for ou while. Quando o programa encontra a instrução break dentro de um loop, ele interrompe a execução do loop e continua com o código que vem após o loop.
+        break
+    else:
+        print("Senha incorreta! Tente novamente.")
