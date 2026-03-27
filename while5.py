da9f9e0c634d1734203210af8354d3f21985bb9a
luisfelipe-fernandesesteves <luisfelipefernandes.corp@gmail.com>
2026-03-27 17:57:06 -0300

    condição, laço de repetição adicionado

--- while5.py
+++ while5.py
@@ -3,7 +3,8 @@ Crie um programa que solicite ao usuário uma senha.
 Enquanto a senha digitada estiver incorreta
 o programa deve continuar pedindo novamente. 
 Quando o usuário acertar, exiba uma mensagem de acesso permitido.
-Regras: Defina uma senha fixa no código 
+
+--Regras: Defina uma senha fixa no código 
 (por exemplo: 1234) 
 Utilize while para repetir até acertar 
 Exemplo:
@@ -13,3 +14,7 @@ Digite a senha: 1234
 Acesso permitido!
 Bora praticar, galera! Ótimos estudos!!!
 '''
+senha = 1234
+
+while senha_digitada != senha:
+   
