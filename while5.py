9af9b137a52f4c94e491d552fe9b5502582af77c
luisfelipe-fernandesesteves <luisfelipefernandes.corp@gmail.com>
2026-03-27 17:53:14 -0300

    documentação do código adicionada

---
+++ while5.py
@@ -0,0 +1,15 @@
+'''📌 Exercício 5 Validação de senha 
+Crie um programa que solicite ao usuário uma senha. 
+Enquanto a senha digitada estiver incorreta
+o programa deve continuar pedindo novamente. 
+Quando o usuário acertar, exiba uma mensagem de acesso permitido.
+Regras: Defina uma senha fixa no código 
+(por exemplo: 1234) 
+Utilize while para repetir até acertar 
+Exemplo:
+Digite a senha: 1111 
+Senha incorreta! 
+Digite a senha: 1234 
+Acesso permitido!
+Bora praticar, galera! Ótimos estudos!!!
+'''
