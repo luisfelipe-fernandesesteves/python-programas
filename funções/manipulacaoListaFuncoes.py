f410e43119f29adbf7639e0449f7e4cb9805a58f
luisfelipe-fernandesesteves <luisfelipefernandes.corp@gmail.com>
2026-05-08 10:47:44 -0300

    enquanto quantidade de alunos for menor que 1 ele continuara perguntando (return qtd)

--- funções/manipulacaoListaFuncoes.py
+++ funções/manipulacaoListaFuncoes.py
@@ -1,5 +1,8 @@
 def qtd_alunos():
     print('-- quantidade de alunos --')
-
-    qtd = int(input('qtd de alunos'))
-    
+    qtd = 0
+    while qtd < 1:
+        qtd = int(input('qtd de alunos'))
+        if qtd < 1:
+            print('quantidade precisa ser MAIOR do que ZERO: 0')
+    return qtd
