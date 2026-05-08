b79752668d6208ad763e464a62dc1f0e0aa32362
luisfelipe-fernandesesteves <luisfelipefernandes.corp@gmail.com>
2026-05-08 10:59:36 -0300

    nome do aluno + 1 adicionado

--- funções/manipulacaoListaFuncoes.py
+++ funções/manipulacaoListaFuncoes.py
@@ -7,10 +7,13 @@ def qtd_alunos():
             print('quantidade precisa ser MAIOR do que ZERO: 0')
     return qtd
 
+#qtd define quantas vezes vai rodar (quantos alunos tem)
 def prencher_notas(qtd):
     print('--preenchendo as notas...--')
     notas = []
     for i in range(qtd):
+        print(f'aluno 1{i+1}:')
         nota = float(input('NOTA:'))
         notas.append(nota)
+        return notas
 
